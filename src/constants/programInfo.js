a57a8befc9d3aa12a5377fd742d5d00da09a3def
Chris DeSilva <chris.a.desilva@gmail.com>
2019-10-02 14:51:32 -0500

    Fill in template with school info

--- src/constants/programInfo.js
+++ src/constants/programInfo.js
@@ -1,4 +1,4 @@
-import logo from "../images/logo_placeholder.png" // add school logo to images folder and import here
+import logo from "../images/logo_vschool.png" // add school logo to images folder and import here
 
 export const applicationsLive = true // set to false if not currently accepting applications
 export const disabledLoanAppFormID = '69140c47-bb03-4a1b-b674-cc136fdea23d' // Hubspot ID for all disabled apply now lead captures
@@ -9,44 +9,44 @@ export const nextCohortStartDate = 'October 29, 2019'
 export const schoolInfo = { // term details section
     interestRate36: '8.99%',
     interestRate60: '10.99%',
-    APRRange36: 'XX.XX - XX.XX%',
-    APRRange60: 'XX.XX - XX.XX%',
+    APRRange36: '11.16 - 11.69%',
+    APRRange60: '12.51 - 12.71%',
     interestOnly: [
         { // for multiple examples of interest only, add items to interestOnly array
-            programName: 'PROGRAM NAME',
-            APR36: 'XX.XX%',
-            financeCharge36: '$X,XXX.XX',
-            IOPayment36: '$XX.XX',
-            FullMonthlyPayment36: '$XXX.XX',
-            APR60: 'XX.XX%',
-            financeCharge60: '$X,XXX.XX',
-            IOPayment60: '$XX.XX',
-            FullMonthlyPayment60: '$XXX.XX',
+            programName: '',
+            APR36: '11.16%',
+            financeCharge36: '$2,293.64',
+            IOPayment36: '$77.91',
+            FullMonthlyPayment36: '$330.67',
+            APR60: '12.51%',
+            financeCharge60: '$4,040.39',
+            IOPayment60: '$95.25',
+            FullMonthlyPayment60: '$226.07',
             LoanExampleAmt: '$10,000',
             LoanExampleOFeeAmt: '$400',
             LoanExampleAmtPlusOFee: '$10,400',
-            programLength: 'X' // program length in months
+            programLength: '3' // program length in months
         }
     ],
     immediateRepayment: [
         { // for multiple examples of immmediate repayment, add items to immediateRepayment array
-            programName: 'PROGRAM NAME',
-            APR36: 'XX.XX%',
-            financeCharge36: '$X,XXX.XX',
-            FullMonthlyPayment36: '$XXX.XX',
-            APR60: 'XX.XX%',
-            financeCharge60: '$X,XXX.XX',
-            FullMonthlyPayment60: '$XXX.XX',
-            LoanExampleAmt: '$10,000',
-            LoanExampleOFeeAmt: '$400',
-            LoanExampleAmtPlusOFee: '$10,400'
+            programName: '',
+            APR36: '11.69%',
+            financeCharge36: '$1,028.20',
+            FullMonthlyPayment36: '$178.56',
+            APR60: '12.71%',
+            financeCharge60: '$1,924.65',
+            FullMonthlyPayment60: '$122.08',
+            LoanExampleAmt: '$5,400',
+            LoanExampleOFeeAmt: '$216',
+            LoanExampleAmtPlusOFee: '$5,616'
         }
     ]
 }
 
 export const faq = { // faq section
-    costOfLiving: false, // true if at least one program has cost of living included
-    costOfLivingPrograms: '', // leave as empty string is cost of living availability is the same across all programs
+    costOfLiving: !false, // true if at least one program has cost of living included
+    costOfLivingPrograms: 'Full Stack JavaScript & User Experience Design Full-Time programs', // leave as empty string is cost of living availability is the same across all programs
     multCostOfLivingPrograms: true, // true if costOfLivingPrograms string has more than one program
     interestOnly: true, // true if interest-only payments are an option
     immediateRepayment: true, // true if immediate repayment is an option
@@ -61,22 +61,22 @@ export const faq = { // faq section
     exampleLoanAmount: '$10,000',
     interestRate36: '8.99%',
     interestRate60: '10.99%',
-    APR36: 'XX.XX - XX.XX%',
-    APR60: 'XX.XX - XX.XX%',
+    APR36: '11.16%',
+    APR60: '12.51%',
     IOPayment36: '$77.91',
     IOPayment60: '$95.25',
 
     // max loan amounts by program for faq1
     loanRange: [
         {
-            programName: "PROGRAM 1",
-            maxAmount: "the max loan amount",
+            programName: "V School's Full Stack JavaScript and User Experience Design Full-Time programs",
+            maxAmount: "$14,900",
             col: true,
-            colAmount: "$6,000"
+            colAmount: "$10,000"
         },
         {
-            programName: "PROGRAM 2",
-            maxAmount: "$10,750",
+            programName: "V School's User Experience Design Part-Time program",
+            maxAmount: "$5,400",
             col: false,
             colAmount: "$6,000"
         },
@@ -89,35 +89,35 @@ export const faq = { // faq section
 
 export const schoolLogo = logo // go to header.js if height needs adjustment
 
-export const schoolName = "SCHOOL_NAME"
+export const schoolName = "V School"
 
-export const schoolURL = 'https://www.SCHOOLWEBSITE.com/' // update with url of school's website
+export const schoolURL = 'https://vschool.io/' // update with url of school's website
 
-export const skfURL = 'https://SCHOOLNAME.skills.fund' // update with Skills Fund url
+export const skfURL = 'https://vschool.skills.fund/' // update with Skills Fund url
 
-export const headline = "Learn to Code at SCHOOL NAME" // update headline as appropriate
+export const headline = "Learn to Code at V School" // update headline as appropriate
 
 export const leadContent = {
     header: "Your last step on the path toward changing your career",
-    paragraph: `${schoolName} designs its programs to cover multiple stacks of programming languages to help graduates develop a diverse skill-set. ${schoolName} partners with Skills Fund to offer tuition${faq.costOfLiving ? ' and cost of living' : ''} financing so more students like you can access their program.`
+    paragraph: `${schoolName} designs its programs to provide students with a foundation for a career in software development. ${schoolName} partners with Skills Fund to offer tuition${faq.costOfLiving ? ' and cost of living' : ''} financing so more students like you can access their program.`
 }
 
 export const threeStepCardText = {
     step1: "",
     step2: {
         header: "select your program",
-        text: "Choose between the Onsite Bootcamp, Online Full-Time Bootcamp, and Online Part-Time Bootcamp programs."
+        text: "Choose between the Full Stack JavaScript, User Experience Design Full-Time, and User Experience Design Part-Time programs."
     },
     step3: `You'll be on your way to an exciting career in tech as part of ${schoolName}'s powerful network.`
 }
 
-export const netlifyFormName = 'SCHOOLNAME_contact'
+export const netlifyFormName = 'vschool_contact'
 
 export const GATracking = 'UA-68312423-1'
 
-export const hubspotFormId = "HUBSPOT_FORM_ID" // create Hubspot form, get form id after publishing
+export const hubspotFormId = "02fbafcf-7867-426d-8d5e-24a965bc6cba" // create Hubspot form, get form id after publishing
 
-export const selectAProgram = "select_a_SCHOOLNAME_program" // update school name to match form field on Hubspot, *** change to "program_name" if only one program ***"
+export const selectAProgram = "select_a_vschool_program" // update school name to match form field on Hubspot, *** change to "program_name" if only one program ***"
 
 // ***** END GENERAL SCHOOL INFO ***** 
 
@@ -134,10 +134,10 @@ export const moreThanSixPrograms = false // set to true if there are 7 or more p
 
 export const programLoanInfo = [ // update with program names and corresponding loan URLs with market segment code from Master Loan Parameters
     {
-        name: "Program 1",
-        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=LENDERCODE1",
+        name: "Full Stack JavaScript",
+        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=SFVS",
         loanInfo: { // match loanInfo in first metro below
-            maxLoanAmt: 19495,
+            maxLoanAmt: 24900,
             loanTerm36: true,
             loanTerm60: true,
             '0': { // interest-only 
@@ -155,7 +155,7 @@ export const programLoanInfo = [ // update with program names and corresponding 
           {
             location: "Metro 1",
             loanInfo: { // // match loanInfo to Program 1 above
-              maxLoanAmt: 19495,
+              maxLoanAmt: 24900,
               loanTerm36: true,
               loanTerm60: true,
               '0': { 
@@ -165,42 +165,14 @@ export const programLoanInfo = [ // update with program names and corresponding 
               },
               '1': null
             }
-          },
-          {
-            location: "Metro 2",
-            loanInfo: {
-                maxLoanAmt: 15545,
-                loanTerm36: true,
-                loanTerm60: true,
-                '0': { 
-                  k: 5, 
-                  apr36: 11.16, 
-                  apr60: 12.51
-                },
-                '1': null
-            }
-          },
-          {
-            location: "Metro 3",
-            loanInfo: {
-                maxLoanAmt: 20545,
-                loanTerm36: true,
-                loanTerm60: true,
-                '0': { 
-                  k: 5, 
-                  apr36: 11.16, 
-                  apr60: 12.51
-                },
-                '1': null
-              },
-            }
+          }
         ]
     },
     {
-        name: "Program 2",
-        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=LENDERCODE2",
+        name: "UX Design Full-Time",
+        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=SKVUE18",
         loanInfo: { // match loanInfo in first metro below
-            maxLoanAmt: 10995,
+            maxLoanAmt: 24900,
             loanTerm36: true,
             loanTerm60: true,
             '0': { 
@@ -218,7 +190,7 @@ export const programLoanInfo = [ // update with program names and corresponding 
             {
               location: "Metro 1",
               loanInfo: { // match loanInfo to Program 2 loanInfo above
-                maxLoanAmt: 10995,
+                maxLoanAmt: 24900,
                 loanTerm36: true,
                 loanTerm60: true,
                 '0': { 
@@ -228,41 +200,23 @@ export const programLoanInfo = [ // update with program names and corresponding 
                 },
                 '1': null
               }
-            },
-            {
-              location: "Metro 2",
-              loanInfo: {
-                  maxLoanAmt: 15545,
-                  loanTerm36: true,
-                  loanTerm60: true,
-                  '0': { 
-                    k: 5, 
-                    apr36: 11.16, 
-                    apr60: 12.51
-                  },
-                  '1': null
-              }
             }
           ]
     },
     {
-        name: "Program 3",
-        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=LENDERCODE3",
+        name: "UX Design Part-Time",
+        url: "https://sf.privateloan.studentloan.org/external/LoanApplication.do?lenderCode=SKVUSR18",
         loanInfo: { // match loanInfo in first metro below
-            maxLoanAmt: 15995,
+            maxLoanAmt: 5400,
             loanTerm36: true,
             loanTerm60: true,
-            '0': { 
-                k: 5, 
-                apr36: 11.16, 
-                apr60: 12.51
-            },
+            '0': null,
             '1': {
-              apr36: 11.25, 
-              apr60: 12.55
+              apr36: 11.69, 
+              apr60: 12.71
           }
         },
-        defaultLoanType: "0", // leave at 0 for interest-only, set to 1 for immediate repayment
+        defaultLoanType: "1", // leave at 0 for interest-only, set to 1 for immediate repayment
         showMetros: false,
         showLoanTypes: false,
         locations: ["Metro A", "Metro B", "Metro C"],
@@ -270,60 +224,22 @@ export const programLoanInfo = [ // update with program names and corresponding 
             {
               location: "Metro A",
               loanInfo: { // match loanInfo to Program 3 loanInfo above
-                maxLoanAmt: 15995,
+                maxLoanAmt: 5400,
                 loanTerm36: true,
                 loanTerm60: true,
-                '0': { 
-                    k: 5, 
-                    apr36: 11.16, 
-                    apr60: 12.51
-                },
+                '0': null,
                 '1': {
-                    apr36: 11.25, 
-                    apr60: 12.55
-                }
+                  apr36: 11.69, 
+                  apr60: 12.71
               }
-            },
-            {
-              location: "Metro B",
-              loanInfo: {
-                  maxLoanAmt: 15545,
-                  loanTerm36: true,
-                  loanTerm60: true,
-                  '0': { 
-                    k: 5, 
-                    apr36: 11.16, 
-                    apr60: 12.51
-                  },
-                  '1': {
-                    apr36: 11.25, 
-                    apr60: 12.55
-                }
               }
-            },
-            {
-              location: "Metro C",
-              loanInfo: {
-                  maxLoanAmt: 20545,
-                  loanTerm36: true,
-                  loanTerm60: true,
-                  '0': { 
-                    k: 5, 
-                    apr36: 11.16, 
-                    apr60: 12.51
-                  },
-                  '1': {
-                    apr36: 11.25, 
-                    apr60: 12.55
-                }
-                },
-              }
+            }
           ]
     }
 ]
 
 // ***** BEGIN LOAN CALC TEXT INFO *****
-export const programMaxText = "Choose the loan amount that works best for you. Borrow up to your metro's max (see table below) for the PROGRAM 1, up to $14,995 for the PROGRAM 2 Bootcamp tuition, and up to $9,995 for the PROGRAM 3 Bootcamp tuition."
+export const programMaxText = "Choose the loan amount that works best for you. Borrow up to $14,900 for tuition & $10,000 for cost of living for the Full Stack JavaScript & User Experience Design Full-Time programs or up to $5,400 for tuition for the UX Part-Time program."
 
 export const paymentTable = {
     headers: ["Program", "Tuition", "Cost of Living", "Max Total"],
